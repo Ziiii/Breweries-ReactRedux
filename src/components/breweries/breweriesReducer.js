cb7bbf60bbc7f93dd2d3adb5a3ef95dc8b8fe50e
Larin Anton <a.larin@itransition.com>
2018-10-08 18:36:52 +0300

    reducer mutation fix

--- src/components/breweries/breweriesReducer.js
+++ src/components/breweries/breweriesReducer.js
@@ -3,7 +3,7 @@ import * as types from "../../actions/actionTypes";
 export default function breweriesReducer(state = [], action) {
   switch (action.type) {
     case types.LOAD_BREWERIES_SUCCESS:
-      return action.breweries;
+      return Object.assign([],action.breweries);
     case types.SAVE_BREWERIES_SUCCESS:
       if(!action.brewery){
         return state;
